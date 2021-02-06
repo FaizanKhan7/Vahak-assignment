d475c1382ebd8f4df4eea1295b2b85c4d4ae8fc1
Faizan khan <mohamedfaizankhan1@gmail.com>
2021-02-06 21:36:53 +0530

    added pricing input

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,11 +2,14 @@ import React from 'react'
 import Heading from '../Common/Heading/Heading'
 import Input from '../Common/Input/Input'
 import styles from "./form.module.css"
-
 function Form() {
     return (
         <div className={styles.formStyles}>
            <Heading title={"Place your Load Bid"}/>
+           <div className={styles.bid}>
+            <label>₹</label>
+            <p contenteditable="true">0</p>
+           </div>
            <Input lableName={"Enter your mobile number*"}/>
            <Input lableName={"Enter your name*"}/>
            <Input lableName={"Enter Remarks (optional)*"}/>
